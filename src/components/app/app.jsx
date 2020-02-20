bea203156d486c0d36ee949480165efe1cda444a
Роман <kromanjs@gmail.com>
2020-02-20 12:17:08 +0200

    3.12 Подготовка

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,22 +1,45 @@
 import React from "react";
 import PropTypes from "prop-types";
+import {Switch, Route, BrowserRouter} from "react-router-dom";
 import Main from "../main/main.jsx";
+import OfferDetails from "../offer-details/offer-details.jsx";
 
 
 const handleOfferTitleClick = () => {};
 
-const App = ({offers}) => {
-  return (
-    <Main
-      offers={offers}
-      onOfferTitleClick={handleOfferTitleClick}
-    />
-  );
-};
+
+export default class App extends React.PureComponent {
+  constructor(props) {
+    super(props);
+  }
+
+  _renderApp() {
+    const {offers} = this.props;
+
+    return (
+      <Main
+        offers={offers}
+        onOfferTitleClick={handleOfferTitleClick}
+      />
+    );
+  }
+
+  render() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            {this._renderApp()}
+          </Route>
+          <Route exact path="/dev-offer">
+            <OfferDetails />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    );
+  }
+}
 
 App.propTypes = {
   offers: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
-
-
-export default App;
