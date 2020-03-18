61fd8064a1c749281a4d3d9f6fbb36b307ed9c4c
Роман <kromanjs@gmail.com>
2020-03-18 19:44:22 +0200

    7.25 Add new selector

--- src/selectors/selectors.js
+++ src/selectors/selectors.js
@@ -5,6 +5,7 @@ import {mapToClient} from '../adapter.js';
 export const getOffers = (state) => state.offers;
 export const getCitiesList = (state) => state.cities.cities;
 export const getCurrentCity = (state) => state.cities.currentCity;
+export const getAuthorizationStatus = (state) => state.authorization.authorizationStatus;
 
 export const getMappedOffers = createSelector(
     getOffers,
