ba30c5b2933fc5dd232ac737895785a8bb859616
Роман <kromanjs@gmail.com>
2020-03-18 19:44:47 +0200

    7.25 Add new operations

--- src/operations/operations.js
+++ src/operations/operations.js
@@ -1,4 +1,5 @@
 import * as actions from '../actions/actions.js';
+import {AuthorizationStatus} from '../const.js';
 
 
 export const loadOffers = () => (dispatch, getState, api) => {
@@ -6,5 +7,27 @@ export const loadOffers = () => (dispatch, getState, api) => {
     .get(`/hotels`)
     .then((response) => {
       dispatch(actions.loadOffers(response.data));
+    });
+};
+
+export const checkAuth = () => (dispatch, getState, api) => {
+  return api
+    .get(`/login`)
+    .then(() => {
+      dispatch(actions.requireAuthorization(AuthorizationStatus.AUTH));
+    })
+    .catch((err) => {
+      throw err;
+    });
+};
+
+export const login = (authData) => (dispatch, getState, api) => {
+  return api
+    .post(`/login`, {
+      email: authData.login,
+      password: authData.password,
+    })
+    .then(() => {
+      dispatch(actions.requireAuthorization(AuthorizationStatus.AUTH));
     });
 };
