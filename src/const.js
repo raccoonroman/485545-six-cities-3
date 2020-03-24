28f5365ac5c6969c39cb8acf06c06cd74c47909c
Роман <kromanjs@gmail.com>
2020-03-24 12:28:07 +0200

    8.28 Add <PrivateRoute />

--- src/const.js
+++ src/const.js
@@ -1,19 +1,19 @@
-const OFFER_CATEGORIES = [`apartment`, `room`, `house`, `hotel`];
-const DEFAULT_CITIES = [`Paris`, `Cologne`, `Brussels`, `Amsterdam`, `Hamburg`, `Dusseldorf`];
+export const OFFER_CATEGORIES = [`apartment`, `room`, `house`, `hotel`];
+export const DEFAULT_CITIES = [`Paris`, `Cologne`, `Brussels`, `Amsterdam`, `Hamburg`, `Dusseldorf`];
 
-const SortType = {
+export const SortType = {
   POPULAR: `Popular`,
   PRICE_LOW_TO_HIGH: `Price: low to high`,
   PRICE_HIGH_TO_LOW: `Price: high to low`,
   TOP_RATED_FIRST: `Top rated first`,
 };
 
-const CardType = {
+export const CardType = {
   CITY: `city`,
   NEAR: `near`,
 };
 
-const ActionType = {
+export const ActionType = {
   LOAD_OFFERS: `LOAD_OFFERS`,
   SET_CITY: `SET_CITY`,
   REQUIRED_AUTHORIZATION: `REQUIRED_AUTHORIZATION`,
@@ -21,16 +21,12 @@ const ActionType = {
   LOAD_COMMENTS: `LOAD_COMMENTS`,
 };
 
-const AuthorizationStatus = {
+export const AuthorizationStatus = {
   AUTH: `AUTH`,
   NO_AUTH: `NO_AUTH`,
 };
 
-export {
-  OFFER_CATEGORIES,
-  DEFAULT_CITIES,
-  SortType,
-  CardType,
-  ActionType,
-  AuthorizationStatus,
+export const AppRoute = {
+  ROOT: `/`,
+  LOGIN: `/login`,
 };
